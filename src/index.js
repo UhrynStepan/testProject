9ac618449f62ace0d942c449cd96584b06a748a0
Stepa Uhryn <stepan.uhryn96@gmail.com>
2018-08-16 15:50:27 +0300

    new

---
+++ src/index.js
@@ -0,0 +1,26 @@
+function handleLogin() {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+
+    console.log('email: ', email);
+    console.log('password: ', password);
+}
+
+function logine() {
+    var name = document.getElementById('name').value;
+    var surname = document.getElementById('surname').value;
+    var email = document.getElementById('email').value;
+    var country = document.getElementById('country').value;
+    var city = document.getElementById('city').value;
+    var phone = document.getElementById('phone').value;
+    var date = document.getElementById('date').value;
+
+    console.log('name: ', name);
+    console.log('surname: ', surname);
+    console.log('email: ', console);
+    console.log('email: ', email);
+    console.log('country: ', country);
+    console.log('city: ', city);
+    console.log('phone: ', phone);
+    console.log('date: ', date);
+}
